8703b4e9c3c6734405cab84a514e5aaf8bf90deb
nisumconsulting <nisumconsulting@192.168.0.2>
2017-04-12 12:24:36 +0530

    committtttt.....

--- src/main/java/com/sampleApache/RestToQueue.java
+++ src/main/java/com/sampleApache/RestToQueue.java
@@ -2,7 +2,7 @@ package com.sampleApache;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class QueueToRest {
+public class RestToQueue {
 	
 	public static void main(String []s) throws Exception{
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-QueueToRest.xml");
